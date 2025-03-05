3bab587fc6a823f40eec356e4f899eb040a2f40c
arcade15xyz <arcade15xyz@gmail.com>
2025-03-05 16:57:58 +0530

    Edit forms

--- routes/web.php
+++ routes/web.php
@@ -8,27 +8,36 @@ use App\Models\Task;
 
 
 
-Route::get('/',function(){
+Route::get('/', function () {
     return redirect()->route('tasks.index');
 });
 
 
 
-Route::get('/tasks', function ()  {
-    return view('index',[
-      'tasks' => Task::latest()->get()
+Route::get('/tasks', function () {
+    return view('index', [
+        'tasks' => Task::latest()->get()
     ]);
 })->name('tasks.index');
 
 Route::view('/tasks/create', 'create')->name('tasks.create');
 
-Route::get('/tasks/{id}', function($id) {
 
-    return view('show',['task'=> Task::findOrFail($id)]);
+Route::get('/tasks/{id}/edit', function ($id) {
+
+    return view('edit', ['task' => Task::findOrFail($id)]);
+})->name('tasks.edit');
+
+Route::get('/tasks/{id}', function ($id) {
+
+    return view('show', ['task' => Task::findOrFail($id)]);
 })->name('tasks.show');
 
 
-Route::post('/tasks',function(Request $request){
+
+
+
+Route::post('/tasks', function (Request $request) {
     $data = $request->validate([
         'title' => 'required|max:255',
         'description' => 'required',
@@ -42,9 +51,28 @@ Route::post('/tasks',function(Request $request){
 
     $task->save();
 
-    return redirect()->route('tasks.show',['id' => $task->id])
-    ->with('success','Task created successfully!');
+    return redirect()->route('tasks.show', ['id' => $task->id])
+        ->with('success', 'Task created successfully!');
 })->name('tasks.store');
+
+
+Route::put('/tasks/{id}', function ($id,Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+
+    $task = Task::findOrFail($id);
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id])
+        ->with('success', 'Task updated successfully!');
+})->name('tasks.update');
 
 // Route::get('/hello ', function () {
 //     return 'Hello';
