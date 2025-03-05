7f53f75eab294fdbfa33f1d852291abaa0d42024
arcade15xyz <arcade15xyz@gmail.com>
2025-03-05 12:10:50 +0530

    Model and reading data

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
-@extends('layouts.app');
+@extends('layouts.app')
 
-@section('title',    'The list of tasks!');
+@section('title',    'The list of tasks!')
 
 
 
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title',$task->title);
+@section('title',$task->title)
 
 
 @section('content')
