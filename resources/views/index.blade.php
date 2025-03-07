d65605bd13f774af7fe5c95b1657715490d1bdf7
arcade15xyz <arcade15xyz@gmail.com>
2025-03-07 11:51:06 +0530

    Styling The Task Page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,14 +6,14 @@
 
 @section('content')
     <nav class=" mb-4">
-        <a href="{{ route('tasks.create') }}" class='text-gray-700 font-medium underline decoration-pink-700'>Add Task</a>
+        <a href="{{ route('tasks.create') }}" class='link'>Add Task</a>
     </nav>
 
     {{-- @if (count($tasks)) --}}
         @forelse ($tasks as $task)
             <div>
                 <a href="{{route('tasks.show',['task'=> $task->id])}}"
-                @class(['font-bold' ,'line-through' => $task->completed])>{{$task->title}}</a>
+                @class(['line-through' => !$task->completed])>{{$task->title}}</a>
             </div>
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel 11 Task List App</title>
     <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+
+    {{-- blade-formatter-disable --}}
+    <style type="text/tailwindcss">
+        .btn{
+            @apply  rounded-md px-2 py-1 text-center font-medium shadow-sm ring-1 ring-slate-700 hover:bg-slate-500 hover:text-white transition-colors duration-500
+        }
+
+        .link{
+            @apply text-gray-700 font-medium underline decoration-pink-500
+        }
+    </style>
+    {{-- blade-formatter-enable --}}
     @yield('styles')
 </head>
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
